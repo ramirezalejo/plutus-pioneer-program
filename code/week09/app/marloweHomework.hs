94e9b50954c2652f29490619f6199013c593a6d6
ramirezalejo <ramirezalejo@live.com>
2021-06-12 17:04:34 -0400

    Homework week09

---
+++ code/week09/app/marloweHomework.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Language.Marlowe.Extended
+
+main :: IO ()
+main = print . pretty $ contract "Charles" "Simon" "Alex" $ Constant 100
+
+choiceId :: Party -> ChoiceId
+choiceId p = ChoiceId "Winner" p
+
+contract :: Party -> Party -> Party -> Value -> Contract
+contract alice bob charlie deposit =
+    When
+        [Case
+            (Deposit
+                charlie
+                charlie
+                ada
+                (MulValue deposit $ Constant 2)
+            )
+            (When
+                [ f alice bob
+                , f bob alice
+                ]
+                10 Close
+            )
+        ]
+        5 Close
+    
+  where
+    f :: Party -> Party -> Case
+    f x y =
+        Case
+            (Deposit
+                x
+                x
+                ada
+                deposit
+            )
+            (When
+                [Case
+                    (Deposit
+                        y
+                        y
+                        ada
+                        deposit
+                    )
+                    (When
+                        [Case
+                            (Choice
+                                (choiceId charlie)
+                                [Bound 1 2]
+                            )
+                            (If
+                                (ValueEQ
+                                    (ChoiceValue $ choiceId charlie)
+                                    (Constant 1)
+                                )
+                                (Pay
+                                    bob
+                                    (Account alice)
+                                    ada
+                                    deposit
+                                    Close
+                                )
+                                (Pay
+                                    alice
+                                    (Account bob)
+                                    ada
+                                    deposit
+                                    Close
+                                )
+                            )]
+                        30 split
+                                
+                    )]
+                20 Close
+            )
+    
+    split :: Contract
+    split = (Pay
+                charlie
+                (Account bob)
+                ada
+                deposit
+                (Pay
+                    charlie
+                    (Account alice)
+                    ada
+                    deposit
+                    Close
+                )
+            )
